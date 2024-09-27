7e6d762ae5982ff3ebe13d9b607a630a353c66bf
EliseR10 <163696607+EliseR10@users.noreply.github.com>
2024-09-27 21:25:29 +0100

    database done + welcome and home page

--- E-commerceApp/e-commerce/src/App.js
+++ E-commerceApp/e-commerce/src/App.js
@@ -1,10 +1,23 @@
+import React from 'react';
 import './App.css';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+
+/*Import my files*/
+import Welcome from './welcome';
+import Home from './home';
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <>
+  {/* Wrap this Root Route to create Router here */}
+  <Route path="/" element={ <Welcome/> }/>
+  <Route path="/home" element={ <Home/> }/>
+  </>
+  
+))
 
 function App() {
   return (
-    <div className="App">
-      <p>Let the fun begin! Shortly</p>
-    </div>
+      <RouterProvider router={router}/>
   );
 }
 
